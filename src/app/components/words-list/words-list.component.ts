6a64ecfca9ac04be4b34ebc5c085662259d7d5e1
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-09-19 13:30:08 +0200

    implemented Google API & word extraction from google spreadsheet, concealed API key as well

--- src/app/components/words-list/words-list.component.ts
+++ src/app/components/words-list/words-list.component.ts
@@ -1,21 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {Exercise1Component} from "../exercise-1/exercise-1.component";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Exercise1Component } from '../exercise-1/exercise-1.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-words-list',
   standalone: true,
-  imports: [CommonModule, Exercise1Component],
+  imports: [CommonModule, Exercise1Component, HttpClientModule],  // Add HttpClientModule here
   templateUrl: './words-list.component.html',
   styleUrls: ['./words-list.component.css']
 })
 export class WordsListComponent implements OnInit {
-  wordsDatabase: string[] = ["Thank you", "ありがとう",
-                             "I see", "そっか",
-                             "My name is", "私の名前は",
-                             "Damn", "くそ",
-                             "Pervert", "変態"];
+  wordsDatabase: string[] = [];
+  apiKey: string = environment.apiKey;
+  spreadsheetID: string = environment.spreadsheetID;
+  sheetRange: string = environment.sheetRange;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getWords();
   }
+
+  getWords(): void {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetID}/values/${this.sheetRange}?key=${this.apiKey}`;
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        if (response.values) {
+          const rows = response.values;
+          this.wordsDatabase = rows.flat();
+          console.log('Successfully extracted Google Sheet data:', this.wordsDatabase);
+        } else {
+          console.error('No data found in response:', response);
+        }
+      },
+      (error) => {
+        console.error('Error fetching the Google Sheet data:', error);
+      }
+    );
+  }
+
 }
