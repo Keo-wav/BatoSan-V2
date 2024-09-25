7c4f37e74ec893f182a37e9821310cd1ef89feec
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-09-25 17:45:22 +0200

    last save before V3 from scratch

--- src/app/components/dictionary/dictionary.component.ts
+++ src/app/components/dictionary/dictionary.component.ts
@@ -1,14 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.css'
 })
 export class DictionaryComponent implements OnInit {
   @Input() wordsDatabase: string[] = [];
+  @Input() englishWords: string[] = [];
+  @Input() japaneseWords: string[] = [];
 
   ngOnInit(): void {
     console.log(this.wordsDatabase);
